ea0263b420e78f3ec6d4024cf1c0e1a4c2feef4a
pershyfon <sabrina.aviana@ui.ac.id>
2024-04-17 15:31:23 +0700

    initializing repo for status/tracking order use case

---
+++ src/main/java/id/ac/ui/cs/advprog/toytopiaorder/ToytopiaOrderApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.toytopiaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToytopiaOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToytopiaOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/toytopiaorder/ToytopiaOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.toytopiaorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToytopiaOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
