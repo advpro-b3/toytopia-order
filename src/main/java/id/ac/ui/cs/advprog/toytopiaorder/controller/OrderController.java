2f2b775aafa00652e5c000fe5f7365ca6b4113d5
pershyfon <sabrina.aviana@ui.ac.id>
2024-05-01 19:04:49 +0700

    [GREEN] OrderServiceImpl and OrderController implementation

--- src/main/java/id/ac/ui/cs/advprog/toytopiaorder/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopiaorder/controller/OrderController.java
@@ -1,1 +1,70 @@
+package id.ac.ui.cs.advprog.toytopiaorder.controller;
 
+import id.ac.ui.cs.advprog.toytopiaorder.model.Cart;
+import id.ac.ui.cs.advprog.toytopiaorder.model.Order;
+import id.ac.ui.cs.advprog.toytopiaorder.service.CartService;
+import id.ac.ui.cs.advprog.toytopiaorder.service.OrderService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Iterator;
+import java.util.List;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private final OrderService orderService;
+    private final CartService cartService;
+
+    public OrderController(OrderService orderService, CartService cartService) {
+        this.orderService = orderService;
+        this.cartService = cartService;
+    }
+
+    @PostMapping("/checkout/{cartId}")
+    public ResponseEntity<String> checkout(@PathVariable("cartId") String cartId) {
+        Cart cart = cartService.getCartById(cartId);
+        if (cart != null) {
+            Order order = orderService.createOrderFromCart(cart);
+            if (order != null) {
+                return ResponseEntity.ok("Order created successfully with ID: " + order.getId());
+            } else {
+                return ResponseEntity.badRequest().body("Failed to create order from cart.");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("Cart with ID " + cartId + " not found.");
+        }
+    }
+
+    @PutMapping("/verify/{id}")
+    public void verifyOrder(@PathVariable("id") String orderId) {
+        orderService.verifyOrder(orderId);
+    }
+
+    @PutMapping("/cancel/{id}")
+    public void cancelOrder(@PathVariable("id") String orderId) {
+        orderService.cancelOrder(orderId);
+    }
+
+    @PutMapping("/setDeliveryMethod/{id}")
+    public void setDeliveryMethod(@PathVariable("id") String orderId, String deliveryMethod) {
+        orderService.setDeliveryMethod(orderId, deliveryMethod);
+    }
+
+    @PutMapping("/complete/{id}")
+    public void completeOrder(@PathVariable("id") String orderId) {
+        orderService.completeOrder(orderId);
+    }
+
+    @GetMapping("/list")
+    public String OrderListPage(Model model) {
+        List<Order> allOrders = orderService.findAll();
+        model.addAttribute("orders", allOrders);
+        return "OrderList";
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/toytopiaorder/service/OrderServiceImpl.java
@@ -0,0 +1,72 @@
+package id.ac.ui.cs.advprog.toytopiaorder.service;
+
+import id.ac.ui.cs.advprog.toytopiaorder.model.Cart;
+import id.ac.ui.cs.advprog.toytopiaorder.model.Order;
+import id.ac.ui.cs.advprog.toytopiaorder.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public void verifyOrder(String orderId) {
+        Order order = orderRepository.findById(orderId);
+        order.verify();
+        orderRepository.save(order);
+    }
+
+    @Override
+    public void cancelOrder(String orderId) {
+        Order order = orderRepository.findById(orderId);
+        order.cancel();
+        orderRepository.save(order);
+    }
+
+    @Override
+    public void setDeliveryMethod(String orderId, String deliveryMethod) {
+        Order order = orderRepository.findById(orderId);
+        order.setDeliveryMethod(deliveryMethod);
+        orderRepository.save(order);
+    }
+
+    @Override
+    public void completeOrder(String orderId) {
+        Order order = orderRepository.findById(orderId);
+        order.complete();
+        orderRepository.save(order);
+    }
+
+    @Override
+    public Order findOrderById(String id) {
+        return orderRepository.findById(id);
+    }
+
+    @Override
+    public List<Order> findAll() {
+        Iterator<Order> orderIterator = orderRepository.findAll();
+        List<Order> allOrder = new ArrayList<>();
+        orderIterator.forEachRemaining(allOrder::add);
+        return allOrder;
+    }
+
+    @Override
+    public Order createOrderFromCart(Cart cart) {
+        if (cart != null) {
+            Order order = new Order(cart);
+            orderRepository.save(order);
+            return order;
+        }
+        return null;
+    }
+}
